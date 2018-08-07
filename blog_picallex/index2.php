30907abb9f866b5eda1d0deae4c254d30273917f
unknown <PriscilaCe>
2018-08-07 18:51:23 -0300

    primera rama

---
+++ blog_picallex/index2.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>BLOG</title>
+        <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="css/bootstrap.min.css">
+            <link href="css/style.css?1.0.0" rel="stylesheet">
+            <link href="css/responsive.css" rel="stylesheet">
+            <link rel="shortcut icon" type="image/x-icon" href="img/PICALLEX-WrtcOff.png">
+    </head>
+    <body>   
+        <div class="contenedor">
+            <nav  class="navbar blog_navbar navbar-expand-lg navbar-light bg-light">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                    <a class="navbar-brand" href="#">PICALLEX</a>
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                    </ul>
+                </div>
+
+                <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2"   aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">IR</button>
+                 </form>
+            </nav>
+            
+            <div class="blog_row">
+                <section >
+                    
+
+                    <div class="relative">
+                        
+                        <div class="cajita">
+                            <div class="pinterest item1"> 
+                                <img src="img/6.jpg" alt="">
+                                <p>Un perro, un camion, un ave, una flor, etc</p>
+                                <a href="novedades.php"> ver más</a>
+                            </div>
+
+                            <div class="pinterest item2">
+                                <img src="img/6.jpg" alt="">
+                                <p>fjebfjaklbfusjlfbcsakñdsnñdsnfkdsds
+                                    nvkdslnvds</p>
+                                <a href="novedades.php"> ver más</a>
+                            </div>
+                                
+                            <div class="pinterest item3">
+                                <img src="img/6.jpg" alt="">
+                                <p>fjebfjaklbfusjlfbcsakñdsnñdsnfkdsds
+                                    nvkdslnvds</p>
+                                <a href="novedades.php"> ver más</a>
+                            </div>
+                                
+                            <div class="pinterest item4"> 
+                                <img src="img/6.jpg" alt="">
+                                <p>fjebfjaklbfusjlfbcsakñdsnñdsnfkdsds
+                                    nvkdslnvds</p>
+                                <a href="VER MAS"> ver más</a>
+                            </div>
+                                
+                            <div class="pinterest item5">
+                                <img src="img/6.jpg" alt="">
+                                <p>fjebfjaklbfusjlfbcsakñdsnñdsnfkdsds
+                                    nvkdslnvds</p>
+                                <a href="VER MAS"> ver más</a>
+                            </div>
+                                
+                            <div class="pinterest item6">
+                                <img src="img/6.jpg" alt="">
+                                <p>fjebfjaklbfusjlfbcsakñdsnñdsnfkdsds
+                                    nvkdslnvds</p>
+                                <a href="VER MAS"> ver más</a> 
+                            </div>
+
+                            <div class="pinterest item7">
+                                <img src="img/6.jpg" alt="">
+                                <p>fjebfjaklbfusjlfbcsakñdsnñdsnfkdsds
+                                    nvkdslnvds</p>
+                                <a href="VER MAS"> ver más</a>
+                            </div>
+                                
+                            
+                        </div>
+                    </div>
+                        
+                </section>
+                <aside>
+                    <div class="box">
+                        <h3>TOP NOVEDADES</h3>
+                        <div class="caja">
+                            <img src="img/prelead.png" class="img-circular">
+                        </div>
+
+                        <div class="caja">
+                            <img src="img/prelead.png" class="img-circular">
+
+                        </div>
+
+                    </div>
+                    <div class="box2">
+                        <div class="box_buscar">
+
+                            <h4> Buscar </h4>
+                        </div>
+                        <div class="box_buscar">
+                           <h5> HERRAMIENTAS </h5>
+                        </div>
+                    </div>
+                            
+                </aside>
+
+            </div>  
+        </div>
+        <script src="js/jquery-3-3-1.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
+
+
+
+
+
+
+            
+        
