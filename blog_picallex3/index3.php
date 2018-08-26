f591ae9b8e6e82a7357cf04b36130bf6d6226594
unknown <PriscilaCe>
2018-08-26 19:12:18 -0300

    blog_picallex no subir

---
+++ blog_picallex3/index3.php
@@ -0,0 +1,105 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style2.css">
+    <link href="https://fonts.googleapis.com/css?family=Inconsolata:400,700" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <div class="blog_container">
+        <nav class="blog_nav_container">
+            <div>
+                <a href="#">Picallex</a>
+            </div>
+            <div>
+                <i class="fa fa-bars" aria-hidden="true"></i>
+            </div>
+        </nav> 
+    
+        <div class="blog_content">
+            <section class="timeline">
+                <ul>
+                    <li>
+                        <div class="pinteres">
+                            <time> 2018 <time>
+                            Primer linea
+                            
+                            <img src="img/prelead.png" alt="" class="img">
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <time>2018 <time>
+                            Primer linea
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <time> 2018 <time>
+                            Primer linea
+                            <img src="img/prelead.png" alt="" class="img">
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <time> 2018 <time>
+                            Primer linea
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <time> 2018 <time>
+                            Primer linea
+                            <img src="img/prelead.png" alt="" class="img">
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <time> 2018 <time>
+                            Primer linea
+                        </div>
+                    </li>
+                </ul>
+                
+            </section>
+    
+            <aside>
+                <div class="blog_column_tools">
+                    <nav class="blog_tools_title">
+                        TOP NOVEDADES
+                    </nav>
+
+                    <div class="blog_box_item">
+                        <div class="blog_items">
+                            <figure>
+                                <img src="img/prelead.png" alt="">
+                            </figure>
+                            <div>
+                                <h1>Titulo de la novedad</h1>
+                                <p>All their equipment and instruments are alive. The sky was cloudless and of a deep dark blue. She stared through the window at the stars.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="box2">
+                        <div class="box_buscar">
+
+                            <h4> Buscar </h4>
+                        </div>
+                        <div class="box_buscar">
+                           <h5> HERRAMIENTAS </h5>
+                        </div>
+                    </div>
+                </div>
+
+            </aside>
+        </div>
+    </div>
+</body>
+</html>
